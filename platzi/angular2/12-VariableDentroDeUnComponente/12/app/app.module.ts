3aea80830e3c20823fb1ccd1e2cd51b7114de15a
Juan Esteban <estebancastanorincon@gmail.com>
2017-01-08 00:02:45 -0500

    Clase 12

---
+++ platzi/angular2/12-VariableDentroDeUnComponente/12/app/app.module.ts
@@ -0,0 +1,18 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+
+import {AppComponent} from './components/app.component';
+import {CoursesComponent } from './components/courses.component'
+
+@NgModule({
+  imports : [
+    BrowserModule
+  ],
+  declarations:[
+    AppComponent,
+    CoursesComponent
+  ],
+  bootstrap: [ AppComponent]
+})
+
+export class AppModule{}
